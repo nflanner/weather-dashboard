ca5031d9850e7151ef1c56936e12a24812ccbaa6
Nolan Flannery <nolanflannery@gmail.com>
2022-05-15 12:36:28 -0400

    added in history and dynamic titles

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,26 @@
 var submitCityEl = document.querySelector('#city-input');
 var submitBtnEl = document.querySelector('#submit');
+var historyDivElement = document.querySelector('#history');
 var apiKey = '4a2fb25bb7a6dbb5c6ef6fca5eb312bc';
 var limit = 5;
+var cityHistory = [];
 
 function handleSubmit(event) {
     event.preventDefault();
     var cityInput = submitCityEl.value;
     // console.log(cityInput);
     // get geo location, then weather
-    getGeoLocation(cityInput)
+    // getGeoLocation(cityInput) 
+    addHistory(cityInput);
+}
+
+function handleHistory(event) {
+    event.preventDefault();
+    var element = event.target;
+    if (element.getAttribute('class') == 'btn btn-block bg-secondary') {
+        var city = element.textContent;
+        // getGeoLocation(city);
+    }
 }
 
 function getGeoLocation(city) {
@@ -29,16 +41,18 @@ function getWeather(lat, lon) {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            document.getElementsByTagName('h1')[0].textContent = city + '(date)' + ' (emoji)';
             document.querySelector('#temp').textContent = convToF(data.current.temp);
             document.querySelector('#wind').textContent = data.current.wind_speed;
             document.querySelector('#humidity').textContent = data.current.humidity;
             document.querySelector('#uvi').textContent = data.current.uvi;
 
             for (var i = 0; i < 5; i++) {
-                console.log(document.getElementsByClassName('emoji')[i].textContent = data.daily[i].weather[0].icon);
-                console.log(document.getElementsByClassName('temp')[i].textContent = convToF(data.daily[i].temp.day));
-                console.log(document.getElementsByClassName('wind')[i].textContent = data.daily[i].wind_speed);
-                console.log(document.getElementsByClassName('humidity')[i].textContent = data.daily[i].humidity);
+                document.getElementsByTagName('h5')[i].textContent = '(date)';
+                document.getElementsByClassName('emoji')[i].textContent = data.daily[i].weather[0].icon;
+                document.getElementsByClassName('temp')[i].textContent = convToF(data.daily[i].temp.day);
+                document.getElementsByClassName('wind')[i].textContent = data.daily[i].wind_speed;
+                document.getElementsByClassName('humidity')[i].textContent = data.daily[i].humidity;
             }
         })
 }
@@ -47,4 +61,16 @@ function convToF(tempK) {
     return Math.round(((tempK - 273.15) * (9/5) + 32) * 100) / 100;
 }
 
+function addHistory(city) {
+    if (!cityHistory.includes(city)) {
+        var historyButton = document.createElement('button');
+        historyButton.setAttribute('type', 'button');
+        historyButton.setAttribute('class', 'btn btn-block bg-secondary');
+        historyButton.textContent = city;
+        historyDivElement.appendChild(historyButton);
+        cityHistory.push(city);
+    }
+}
+
 submitBtnEl.addEventListener("click", handleSubmit);
+historyDivElement.addEventListener("click", handleHistory);
